5acca9b669b39f4abc9a497e143fc357cebfe4a7
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-02-16 16:21:25 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary(baseSalary, bonus, taxRate) { // creates the function a
 console.log("**Task 1**") // for console readability
 calculateSalary(5000, 500, 0.1) // calls function
 calculateSalary(7000, 1000, 0.15) // calls function
+
+//Task 2 - Product Price After Discount
+function calculateDiscount(price, discountRate) {  //  creates the function and parameters
+    let finalPrice = price - (price * discountRate); // calculates final price
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`) // logs the final price to the console
+};
+console.log("**Task 2**") // for easier console readability
+calculateDiscount(100, 0.2);  // calls function
+calculateDiscount(250, 0.15); // calls function

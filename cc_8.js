d316e1d21282166a701dcedb8be8023ea69b165e
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-02-16 16:22:21 -0500

    Task 3- Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -15,3 +15,14 @@ function calculateDiscount(price, discountRate) {  //  creates the function and 
 console.log("**Task 2**") // for easier console readability
 calculateDiscount(100, 0.2);  // calls function
 calculateDiscount(250, 0.15); // calls function
+
+//Task 3 Service Fee Calculation.
+let calculateServiceFee = (amount, serviceType) => { // creates the arrow function and its parameters
+    if (serviceType === "Premium") serviceFee = amount * 0.15; // calculates service fee for premium plan
+    else if (serviceType === "Standard") serviceFee = amount * .10; // calculates service fee for stanard plan
+    else serviceFee = amount *0.05; // calculates service fee for basic plan as it is the only option left
+    console.log(`Service Fee: $${serviceFee.toFixed(2)}`); // logs service fee to console
+};
+console.log("**Task 3**") //for easier console readability
+calculateServiceFee(200, "Premium") // calls function
+calculateServiceFee(500, "Standard") // calls function
